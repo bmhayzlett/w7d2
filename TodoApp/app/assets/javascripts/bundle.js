e2a1e20d5dddd27db02e36a4479616e8e128d373
App Academy Student <student@appacademy.io>
2016-02-16 15:28:07 -0800

    write functionality for todo_store

--- TodoApp/app/assets/javascripts/bundle.js
+++ TodoApp/app/assets/javascripts/bundle.js
@@ -42,9 +42,80 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	window.TodoStore = __webpack_require__(1);
+
+/***/ },
+/* 1 */
 /***/ function(module, exports) {
 
-
+	var _todos = {};
+	var _callbacks = [];
+	
+	var TodoStore = {
+	
+	  changed: function () {
+	    _callbacks.forEach(function (cb) {
+	      cb.apply(null, _todos);
+	    });
+	  },
+	
+	  addChangedHandler: function (callback) {
+	    _callbacks.push(callback);
+	    this.changed();
+	  },
+	
+	  removeChangedHandler: function (callback) {
+	    var index = _callbacks.indexOf(callback);
+	    _callbacks.splice(index, 1);
+	    this.changed();
+	  },
+	
+	  all: function () {
+	    return _todos;
+	  },
+	
+	  fetch: function () {
+	    $.get("api/todos", function (data) {
+	      _todos = data;
+	    });
+	    this.changed();
+	  },
+	
+	  create: function (todo) {
+	    $.post("api/todos", todo).done(function (data) {
+	      _todos[data.id] = data;
+	    });
+	
+	    this.changed();
+	  },
+	
+	  destroy: function (id) {
+	    if (Object.keys(_todos).includes(id.toString())) {
+	      $.ajax({
+	        url: "/api/todos/" + id,
+	        type: 'post',
+	        data: { _method: 'delete', id: id },
+	        success: function () {
+	          console.log("message deleted");
+	          _todos = _todos.filter(function (obj) {
+	            return obj.id !== id;
+	          });
+	        } });
+	      this.changed();
+	    }
+	  },
+	
+	  toggleDone: function (id) {
+	    var truthy = _todos[id].done;
+	    $.patch("api/todos/" + id, { todo: { done: !truthy } });
+	    this.changed();
+	  }
+	
+	};
+	
+	module.exports = TodoStore;
 
 /***/ }
 /******/ ]);
--- TodoApp/app/controllers/api/todos_controller.rb
+++ TodoApp/app/controllers/api/todos_controller.rb
@@ -1,16 +1,24 @@
 class Api::TodosController < ApplicationController
   def create
-    Todo.create!(todo_params)
+    output = Todo.create!(todo_params)
+    render :json => output
   end
 
   def update
     current_todo = Todo.find_by(params[:id])
     current_todo.update!(todo_params)
+    redirect_to root_url
   end
 
   def destroy
     current_todo = Todo.find_by(params[:id])
     current_todo.destroy!
+    redirect_to root_url
+  end
+
+  def index
+    todos = Todo.all
+    render :json => todos
   end
 
 
